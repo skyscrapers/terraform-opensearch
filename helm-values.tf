60c4df9e515ec11b1395f9eb1effb4b8187c0395
Adey Merrett <Komorebi-E@users.noreply.github.com>
2018-12-19 11:34:34 +0100

    Fix path not being read as a file

--- helm-values.tf
+++ helm-values.tf
@@ -10,7 +10,7 @@ data "aws_region" "current" {}
 
 data "template_file" "helm_values" {
   count    = "${length(var.prometheus_labels) != 0 ? 1 : 0}"
-  template = "${local.template_filename}"
+  template = "${file("${local.template_filename}")}"
 
   vars {
     elasticsearch_endpoint   = "${local.elasticsearch_endpoint}"
