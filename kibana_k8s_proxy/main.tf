1f01e0e7f03966388e4a7e8ab6fc0fdf72682b4b
Philip Dubois <hello@philipdubois.be>
2019-06-06 17:29:23 +0200

    Add proper vars and fixes

--- kibana_k8s_proxy/main.tf
+++ kibana_k8s_proxy/main.tf
@@ -16,6 +16,11 @@ locals {
   name = "kibana-gatekeeper"
 }
 
+resource "random_string" "encryption_key" {
+  length  = 32
+  special = false
+}
+
 resource "kubernetes_deployment" "gatekeeper" {
   metadata {
     name      = local.name
@@ -45,21 +50,21 @@ resource "kubernetes_deployment" "gatekeeper" {
           name  = local.name
           image = var.gatekeeper_image
           args  = concat([
-            "--listen=0.0.0.0:3000"
-            "--upstream-url=${var.gatekeeper_upstream_url}"
-            "--discovery-url=${var.gatekeeper_discovery_url}"
-            "--redirection-url=${var.gatekeeper_redirection_url}"
-            "--client-id=${var.gatekeeper_client_id}"
-            "--client-secret=${var.gatekeeper_client_secret}"
-            "--enable-refresh-tokens=true"
-            "--encryption-key=${random_string.encryption_key.result}"
-            "--resources=uri=/*|groups=${var.gatekeeper_oidc_groups}"
-            "--scopes=groups"
-            "--upstream-keepalive-timeout=${var.gatekeeper_timeout}"
-            "--upstream-timeout=${var.gatekeeper_timeout}"
-            "--upstream-response-header-timeout=${var.gatekeeper_timeout}"
-            "--server-read-timeout=${var.gatekeeper_timeout}"
-            "--server-write-timeout=${var.gatekeeper_timeout}"
+            "--listen=0.0.0.0:3000",
+            "--discovery-url=${var.gatekeeper_discovery_url}",
+            "--upstream-url=https://${var.elasticsearch_endpoint}",
+            "--redirection-url=https://${var.gatekeeper_ingress_host}",
+            "--client-id=${var.gatekeeper_client_id}",
+            "--client-secret=${var.gatekeeper_client_secret}",
+            "--enable-refresh-tokens=true",
+            "--encryption-key=${random_string.encryption_key.result}",
+            "--resources=uri=/*|groups=${join(",", var.gatekeeper_oidc_groups)}",
+            "--scopes=groups",
+            "--upstream-keepalive-timeout=${var.gatekeeper_timeout}",
+            "--upstream-timeout=${var.gatekeeper_timeout}",
+            "--upstream-response-header-timeout=${var.gatekeeper_timeout}",
+            "--server-read-timeout=${var.gatekeeper_timeout}",
+            "--server-write-timeout=${var.gatekeeper_timeout}",
             "--enable-authorization-header=false"
           ], var.gatekeeper_extra_args)
         }
@@ -81,7 +86,7 @@ resource "kubernetes_service" "gatekeeper" {
   spec {
     type = "ClusterIP"
 
-    selector {
+    selector = {
       app = local.name
     }
 
@@ -101,15 +106,15 @@ resource "kubernetes_ingress" "gatekeeper" {
     }
 
     annotations = {
-      kubernetes.io/ingress.class          = "nginx"
-      kubernetes.io/tls-acme               = "true"
-      nginx.ingress.kubernetes.io/app-root = "/_plugin/kibana/"
+      "kubernetes.io/ingress.class"          = "nginx",
+      "kubernetes.io/tls-acme"               = "true",
+      "nginx.ingress.kubernetes.io/app-root" = "/_plugin/kibana/",
     }
   }
 
   spec {
     rule {
-      host = var.gatekeeper_redirection_url
+      host = var.gatekeeper_ingress_host
 
       http {
         path {
@@ -125,7 +130,7 @@ resource "kubernetes_ingress" "gatekeeper" {
 
     tls {
       secret_name = "${local.name}-tls"
-      hosts = var.gatekeeper_redirection_url
+      hosts = [var.gatekeeper_ingress_host]
     }
   }
 }
--- kibana_k8s_proxy/variables.tf
+++ kibana_k8s_proxy/variables.tf
@@ -13,26 +13,45 @@ variable "kubernetes_namespace" {
   description = "Kubernetes namespace where to deploy the `skyscrapers/elasticsearch-monitoring` chart"
 }
 
-variable "gatekeeper_oidc_groups" {
-  #type        = list(string)
-  description = "Groups that will be granted access to the Kibana dashboard. When using Dex and Kubesignin these will be GitHub teams in the form `<gh_org>:<gh_team>`, for example `skyscrapers:k8s-admins`"
-  default     = []
+variable "gatekeeper_image" {
+  type        = string
+  description = ""
+  default     = "keycloak/keycloak-gatekeeper:6.0.1"
 }
 
+variable "gatekeeper_ingress_host" {
+  type        = string
+  description = "Hostname to use for the Ingress"
+}
 
-          #   "--upstream-url=${var.gatekeeper_upstream_url}"
-          #   "--discovery-url=${var.gatekeeper_discovery_url}"
-          #   "--redirection-url=${var.gatekeeper_redirection_url}"
-          #   "--client-id=${var.gatekeeper_client_id}"
-          #   "--client-secret=${var.gatekeeper_client_secret}"
-          #   "--enable-refresh-tokens=true"
-          #   "--encryption-key=${random_string.encryption_key.result}"
-          #   "--resources=uri=/*|groups=${var.gatekeeper_oidc_groups}"
-          #   "--scopes=groups"
-          #   "--upstream-keepalive-timeout=${var.gatekeeper_timeout}"
-          #   "--upstream-timeout=${var.gatekeeper_timeout}"
-          #   "--upstream-response-header-timeout=${var.gatekeeper_timeout}"
-          #   "--server-read-timeout=${var.gatekeeper_timeout}"
-          #   "--server-write-timeout=${var.gatekeeper_timeout}"
-          #   "--enable-authorization-header=false"
-          # ], var.gatekeeper_extra_args)
+variable "gatekeeper_discovery_url" {
+  type        = string
+  description = "URL for OpenID autoconfiguration"
+}
+
+variable "gatekeeper_client_id" {
+  type        = string
+  description = "Client ID for OpenID server"
+}
+
+variable "gatekeeper_client_secret" {
+  type        = string
+  description = "Client secret for OpenID server"
+}
+
+variable "gatekeeper_oidc_groups" {
+  type        = list(string)
+  description = "Groups that will be granted access. When using Dex with GitHub, teams are defined in the form `<gh_org>:<gh_team>`, for example `skyscrapers:k8s-admins`"
+}
+
+variable "gatekeeper_timeout" {
+  type        = string
+  description = "Upstream timeouts to use for the proxy"
+  default     = "500s"
+}
+
+variable "gatekeeper_extra_args" {
+  type        = list(string)
+  description = "Additional keycloack-gatekeeper command line arguments"
+  default     = []
+}
