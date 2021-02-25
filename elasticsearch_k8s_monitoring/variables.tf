aeb5154860a96330a01ecfd420cfe11b192b8e60
iuri aranda <iuri@skyscrapers.eu>
2021-02-25 11:51:43 +0100

    Update ES monitoring chart version

--- elasticsearch_k8s_monitoring/variables.tf
+++ elasticsearch_k8s_monitoring/variables.tf
@@ -1,7 +1,7 @@
 variable "elasticsearch_monitoring_chart_version" {
   type        = string
   description = "elasticsearch-monitoring Helm chart version to deploy"
-  default     = "1.2.4"
+  default     = "1.2.5"
 }
 
 variable "elasticsearch_endpoint" {
