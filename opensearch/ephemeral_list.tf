89ef7cd7712ba2a44fbb96b75657e9b54db687d8
Philip Dubois <hello@philipdubois.be>
2022-01-05 14:21:10 +0100

    Fix ephemeral_list and override_main_response_version

--- opensearch/ephemeral_list.tf
+++ opensearch/ephemeral_list.tf
@@ -15,6 +15,20 @@ variable "ephemeral_list" {
     "r6gd.4xlarge.search",
     "r6gd.8xlarge.search",
     "r6gd.12xlarge.search",
-    "r6gd.16xlarge.search"
+    "r6gd.16xlarge.search",
+    ## Compatibility
+    "i3.large.elasticsearch",
+    "i3.xlarge.elasticsearch",
+    "i3.2xlarge.elasticsearch",
+    "i3.4xlarge.elasticsearch",
+    "i3.8xlarge.elasticsearch",
+    "i3.16xlarge.elasticsearch",
+    "r6gd.large.elasticsearch",
+    "r6gd.xlarge.elasticsearch",
+    "r6gd.2xlarge.elasticsearch",
+    "r6gd.4xlarge.elasticsearch",
+    "r6gd.8xlarge.elasticsearch",
+    "r6gd.12xlarge.elasticsearch",
+    "r6gd.16xlarge.elasticsearch",
   ]
 }
