e0d5944f3284febfb73c07f4c64d1dbb65c0a38a
Simon Rondelez <simonrondelez@me.com>
2022-11-17 11:40:25 +0100

    Bump elasticsearch-monitoring Helm chart version

--- elasticsearch_k8s_monitoring/variables.tf
+++ elasticsearch_k8s_monitoring/variables.tf
@@ -1,7 +1,7 @@
 variable "elasticsearch_monitoring_chart_version" {
   type        = string
   description = "elasticsearch-monitoring Helm chart version to deploy"
-  default     = "1.8.0"
+  default     = "1.9.0"
 }
 
 variable "elasticsearch_endpoint" {
